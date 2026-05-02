401067068eafac267e0ba473a4f1f90c4ce8223b
pandaSPIRIT <102025936+pandaSPIRIT@users.noreply.github.com>
2026-05-02 15:32:19 +0530

    Add: adapter - submission-0

---
+++ Design Patterns/adapter/submission-0.cpp
@@ -0,0 +1,48 @@
+class Square {
+    double sideLength;
+
+public:
+    Square() : sideLength(0) {}
+
+    Square(double sideLength) : sideLength(sideLength) {}
+
+    virtual double getSideLength() {
+        return sideLength;
+    }
+};
+
+class SquareHole {
+    double sideLength;
+
+public:
+    SquareHole(double sideLength) : sideLength(sideLength) {}
+
+    bool canFit(Square& square) {
+        return sideLength >= square.getSideLength();
+    }
+};
+
+class Circle {
+    double radius;
+
+public:
+    Circle(double radius) : radius(radius) {}
+
+    double getRadius() {
+        return radius;
+    }
+};
+
+class CircleToSquareAdapter : public Square {
+    Square* square;
+public:
+    CircleToSquareAdapter(Circle& circle) {
+      // Write your code here
+      square = new Square(2 * circle.getRadius());
+    }
+
+    double getSideLength() override {
+      // Write your code here
+      return square->getSideLength();
+    }
+};
